b2dff98c2d089774e230bb09915c3395ecd0be01
Thaísa Vieira <thaisa-vieira@outlook.com.br>
2025-10-30 00:22:06 -0300

    Correção ortográfica

--- Algoritmo - Estrutura Sequencial/L1EF1/L1EF1.c
+++ Algoritmo - Estrutura Sequencial/L1EF1/L1EF1.c
@@ -3,7 +3,7 @@
 void main (void)
 {
     float  PrecUnic, QtdeVend, ValPagar;
-    printf("Digite o preco e a quantidade vendida: ");
+    printf("Digite o preço e a quantidade vendida: ");
     scanf("%f %f", &PrecUnic, &QtdeVend);
     ValPagar = PrecUnic * QtdeVend;
     printf("Valor a pagar: R$ %.2f\n",ValPagar );
